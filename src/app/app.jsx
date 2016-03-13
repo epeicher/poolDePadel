bb434d3f21eeedb86cb0cc17d96a5c0b668fa58f
Roberto Aranda <ese.rober@gmail.com>
2016-03-13 22:57:15 +0100

    Router added - initial version

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
 import Main from './Main'; // Our custom react component
 
 //Needed for onTouchTap
@@ -9,19 +10,24 @@ import Main from './Main'; // Our custom react component
 //https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
+/*
+      <Route path="about" component={About}/>
+      <Route path="users" component={Users}>
+        <Route path="/user/:userId" component={User}/>
+      </Route>
+      <Route path="*" component={NoMatch}/>
+*/
 
-// render((
-//   <Router history={browserHistory}>
-//     <Route path="/" component={App}>
-//       <Route path="about" component={About}/>
-//       <Route path="users" component={Users}>
-//         <Route path="/user/:userId" component={User}/>
-//       </Route>
-//       <Route path="*" component={NoMatch}/>
-//     </Route>
-//   </Router>
-// ), document.body)
+
+
+const AppRouter = (props) => {
+  return (<Router history={browserHistory}>
+    <Route path="/" component={Main}>
+     <IndexRoute component={Main}/>
+    </Route>
+  </Router>);
+}
 
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-ReactDOM.render(<Main />, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
