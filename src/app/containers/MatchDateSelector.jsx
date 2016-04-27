50f428736d4531513a9b40c263aed0c1d022ee0b
Roberto Aranda <ese.rober@gmail.com>
2016-04-27 23:13:42 +0200

    First (incomplete) version of select Matches menu

--- src/app/containers/MatchDateSelector.jsx
+++ src/app/containers/MatchDateSelector.jsx
@@ -3,6 +3,10 @@ import {connect} from 'react-redux'
 import AppBar from 'material-ui/lib/app-bar'
 import IconButton from 'material-ui/lib/icon-button'
 import { getNextMatch } from '../actions'
+import IconMenu from 'material-ui/lib/menus/icon-menu'
+import MenuItem from 'material-ui/lib/menus/menu-item'
+import ContentFilter from 'material-ui/lib/svg-icons/content/filter-list'
+import {Link} from 'react-router'
 
 class MatchDateSelector extends React.Component {
     
@@ -17,21 +21,43 @@ class MatchDateSelector extends React.Component {
   componentWillMount() {
       this.props.dispatch(getNextMatch());
   }    
+  
+  handleClick(e,v,c) {
+      // TODO: Add the action duuude
+      console.log(e.target.innerHTML);
+  }
     
     render() {
         const {matchDate} = this.props;
         return (          
             <AppBar 
                 title={"Próximo partido: " + matchDate} 
-                iconElementLeft={<IconButton />} 
-            />
+                iconElementLeft={
+                    <IconMenu iconButtonElement= {
+                        <IconButton><ContentFilter /></IconButton>
+                        }
+                        onItemTouchTap={this.handleClick}
+                    >
+                    {
+                        this.props.matchesDates.map((dt,idx) => {
+                            return (
+                                <MenuItem key={idx} value={dt} primaryText={dt} />    
+                            )
+                        }) 
+                    }                       
+                    </IconMenu>
+                } 
+            >
+
+            </AppBar>
         )
     }
 }
 
 const mapStateToProps = (state) => {
   return {
-      matchDate: state.nextMatch.matchDate
+      matchDate: state.nextMatch.matchDate,
+      matchesDates: ["2016-03-10","2015-03-10"]
   }
 }
 
