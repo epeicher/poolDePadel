f7b91c83c39df335165bab1db998d85b23619a6c
Roberto Aranda <ese.rober@gmail.com>
2016-05-01 21:41:38 +0200

    Create an incomplete Login page

--- src/app/containers/Login.jsx
+++ src/app/containers/Login.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {Card, CardText, RaisedButton} from 'material-ui';
 import login from '../services/login';
 import { browserHistory } from 'react-router'
+import FlatButton from 'material-ui/lib/flat-button'
+import TextField from 'material-ui/lib/text-field'
+import FontIcon from 'material-ui/lib/font-icon'
 
 class Login extends React.Component {
 
@@ -12,22 +15,63 @@ class Login extends React.Component {
     render() {
 
         return (
+          <div>
             <Card style={{
               'maxWidth': '800px',
-              'margin': '30px auto',
+              'margin': '5px auto',
               'padding': '50px'
             }}>
-              <CardText style={{
-                'textAlign': 'center'
-              }}>
-                Please log in with your Google account.
-              </CardText>
-
-              <RaisedButton style={{
-                display: 'block',
-              }} onClick={this.onClick.bind(this)}
-              label="Log in with Google" primary={true} />
+            <form>
+              <TextField
+                id="player"
+                hintText="Nombre"
+                //errorText={(playerName.touched && playerName.error) || error ? playerName.error || error : ''}
+                floatingLabelText="Nombre de usuario"
+              />
+              <br/>
+              <TextField
+                id="pwd"
+                hintText="Contraseña"
+                //errorText={(playerName.touched && playerName.error) || error ? playerName.error || error : ''}
+                floatingLabelText="Contraseña"
+              /> 
+              <br/>
+              <RaisedButton label="Log in" />            
+            </form>
             </Card>
+            <Card style={{
+              'margin': '5px auto',
+              'padding': '20px'
+            }}>
+            <FlatButton
+              label="Crear usuario"
+            />          
+            <br/>
+            <FlatButton
+              label="Recordar contraseña"
+            />
+            </Card>
+            <Card style={{
+              'margin': '5px auto',
+              'padding': '20px'
+            }}>
+            <FlatButton
+              label="Autentícate con Google"
+              linkButton={true}
+              href=""
+              secondary={true}
+              icon={<FontIcon className="muidocs-icon-custom-github" />}
+              onClick={this.onClick.bind(this)}
+            />           
+            <FlatButton
+              label="Autentícate con Facebook"
+              linkButton={true}
+              href="https://github.com/callemall/material-ui"
+              secondary={true}
+              icon={<FontIcon className="muidocs-icon-custom-facebook" />}
+            />           
+            </Card>
+            </div>
         );
     }
 }
